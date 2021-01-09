f0d0f8d23f3e11c78b94809d8f9611869a137b61
Pablo Perez <perez51160900@gmail.com>
2021-01-09 14:46:40 -0400

    inicio

---
+++ webapp.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const aplicacion = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('express-flash')
+
+var pool = mysql.createPool({
+  connectionLimit: 20,
+  host: 'localhost',
+  user: 'root',
+  password: '27784169Pp',
+  database: 'blog_viajes'
+})
+
+aplicacion.use(bodyParser.json())
+aplicacion.use(bodyParser.urlencoded({ extended: true }))
+aplicacion.set("view engine", "ejs")
+aplicacion.use(session({ secret: 'token-muy-secreto', resave: true, saveUninitialized: true }));
+aplicacion.use(flash())
+aplicacion.use(express.static('public'))
+
+aplicacion.get('/', function (peticion, respuesta) {
+  pool.getConnection(function(err, connection) {
+    respuesta.render('index')
+  })
+})
+
+aplicacion.listen(8080, function(){
+  console.log("Servidor iniciado")
+})

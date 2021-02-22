d95babe89df0d3203661e1715fc7b88ee6d699b6
pablo <perez51160900@gmail.com>
2021-02-22 13:57:10 -0400

    modulo de agregar publicacion

--- webapp.js
+++ webapp.js
@@ -100,12 +100,43 @@ aplicacion.post('/procesarR', function(peticion, respuesta){
 })
 
 aplicacion.get('/admin/index', function (peticion, respuesta) {
-  respuesta.render('admin/index', { usuario: peticion.session.usuario, mensaje: peticion.flash('mensaje') })
+  pool.getConnection(function(err, connection){
+    const consulta = `SELECT * FROM publicaciones WHERE autor_id = ${peticion.session.usuario.id}`
+    connection.query(consulta, function(error, filas, campos){
+       respuesta.render('admin/index', { usuario: peticion.session.usuario, mensaje: peticion.flash('mensaje'), publicaciones: filas })
+    })
+   
+    connection.release()
+  })
+  
+})
+
+aplicacion.get('/admin/agregar', function(peticion, respuesta){
+  respuesta.render('admin/agregar', { usuario: peticion.session.usuario, mensaje: peticion.flash('mensaje') })
 })
 
 aplicacion.get('/cerrar', function(peticion, respuesta){
   peticion.session.destroy();
   respuesta.redirect("/")
+})
+
+aplicacion.post('/admin/procesarP', function(peticion, respuesta){
+  pool.getConnection(function(err, connection){
+    const fecha = new Date()
+    const date = `${fecha.getFullYear()}-${fecha.getMonth()+1}-${fecha.getDate()}`
+    const titulo = peticion.body.titulo
+    const contenido = peticion.body.contenido
+    const resumen = peticion.body.resumen
+    console.log(titulo)
+    const query = `INSERT INTO publicaciones (titulo, resumen, contenido, fecha_hora, autor_id) VALUES ('${titulo}', '${resumen}', '${contenido}', '${date}', ${peticion.session.usuario.id})`
+
+    connection.query(query, function(error, filas, campos){
+      peticion.flash('mensaje', 'Publicacion Agregada')
+      respuesta.redirect('/admin/index')
+    })
+
+    connection.release()
+  })
 })
 
 aplicacion.listen(8080, function(){
